ab93a92e7dac17aa04f04659f9d910c70f341ea6
Matthias-iut <matthias.trupin95@gmail.com>
2024-05-21 16:47:02 +0200

    Ajout des class Java BO, DTO et interface

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/QPUG/entities/mappers/MapperBOtoDTO.java
@@ -0,0 +1,17 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.entities.mappers;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.entities.bo.QuestionBO;
+
+public class MapperBOtoDTO {
+    private String cheminVersFichierCSV;
+    public MapperBOtoDTO(String cheminVersFichierCSV){
+        this.cheminVersFichierCSV=cheminVersFichierCSV;
+
+    }
+    /*
+    public QuestionBO chargerQuestionBO(){
+
+    }
+
+     */
+}
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/QPUG/utils/exceptions/FichierManquantException.java
@@ -0,0 +1,7 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.utils.exceptions;
+
+public class FichierManquantException extends Exception{
+    public FichierManquantException(String message){
+        super(message);
+    }
+}
