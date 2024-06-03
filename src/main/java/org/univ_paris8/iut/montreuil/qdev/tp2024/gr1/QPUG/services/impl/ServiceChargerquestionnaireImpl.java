924ed380d55f21f20449da38db96a2ad6b86dd78
Trupin <120423796+Matthias-iut@users.noreply.github.com>
2024-06-03 16:59:06 +0200

    Ajout d'un cas d'usage pour la fonction ChargerQuestionnaire

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/QPUG/services/impl/ServiceChargerquestionnaireImpl.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/QPUG/services/impl/ServiceChargerquestionnaireImpl.java
@@ -7,6 +7,7 @@ import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.entities.dto.Questionn
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.services.models.QuestionnaireInterface;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.utils.exceptions.FichierInaccessibleException;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.utils.exceptions.FichierManquantException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr1.QPUG.utils.exceptions.QuestionnaireManquantException;
 
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -17,17 +18,22 @@ public class ServiceChargerquestionnaireImpl implements QuestionnaireInterface {
     public QuestionnaireDTO chargerQuestionnaire(String cheminVersFichierCSV, int idQuestionnaire) throws FichierInaccessibleException, FichierManquantException {
 
         if (cheminVersFichierCSV.length() == 0)
-            throw new FichierManquantException("Ligne vide rencontrée dans le fichier CSV.");
+            throw new FichierManquantException("Rentrez le bon chemin de fichier : chemin vide.");
         try(CSVReader reader = new CSVReader(new FileReader(cheminVersFichierCSV))){
             List<String[]> lettreSuivante;
 
             lettreSuivante= reader.readAll();
             QuestionnaireDTO questionnaireDTO = new QuestionnaireDTO(Integer.parseInt(lettreSuivante.get(0)[0]));
             for (int ligne=0; ligne<lettreSuivante.size(); ligne++){
-                QuestionBO q = new QuestionBO(lettreSuivante.get(ligne));
-                String[] q2 = q.getQuestionBO();
-                QuestionDTO questionDTO = new QuestionDTO(Integer.parseInt(q2[0]),Integer.parseInt(q2[1]),q2[2],q2[3],q2[4],q2[5],q2[6],q2[7]);
-                questionnaireDTO.addQuestionDTO(questionDTO);
+                if (Integer.parseInt(lettreSuivante.get(ligne)[0]) == idQuestionnaire) {
+                    QuestionBO q = new QuestionBO(lettreSuivante.get(ligne));
+                    String[] q2 = q.getQuestionBO();
+                    QuestionDTO questionDTO = new QuestionDTO(Integer.parseInt(q2[0]),Integer.parseInt(q2[1]),q2[2],q2[3],q2[4],q2[5],q2[6],q2[7]);
+                    questionnaireDTO.addQuestionDTO(questionDTO);
+                }
+            }
+            if (questionnaireDTO.getNbQuestions() < 1) {
+                throw new QuestionnaireManquantException("Le questionnaire que vous essayez de charger n'existe pas dans le fichier.");
             }
             return questionnaireDTO;
             /**
@@ -36,7 +42,7 @@ public class ServiceChargerquestionnaireImpl implements QuestionnaireInterface {
             tableau.add(fichierEnString);
             **/
         }catch(Exception e){
-            throw new FichierManquantException("Le fichier est manquant.");
+            throw new FichierInaccessibleException("Le fichier est inaccessible.");
         }
     }
 }
